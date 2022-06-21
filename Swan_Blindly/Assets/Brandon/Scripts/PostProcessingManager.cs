847f9a0d4a4eaf13166567595122d0a4f5038947
brandonsurin28 <brandonsurin8@gmail.com>
2022-06-22 01:58:47 +0800

    URP Changes

--- Swan_Blindly/Assets/Brandon/Scripts/PostProcessingManager.cs
+++ Swan_Blindly/Assets/Brandon/Scripts/PostProcessingManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class PostProcessingManage : MonoBehaviour
+public class PostProcessingManager : MonoBehaviour
 {
+    public Volume volume;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Bloom bloom;
+
+        if(volume.profile.TryGet<Bloom>(out bloom))
+        {
+            bloom.intensity.value = 100;
+        }
     }
 
     // Update is called once per frame
