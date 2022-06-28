8c858e541970b957441099d711b7a1b54b100c75
Vinnaeysh <0128028@kdu-online.com>
2022-06-28 17:06:10 +0800

    Made the fixed touch field transparent

--- Swan_Blindly/Assets/Vinnaeysh/Scripts/Camera/New Test Scripts/CineTOuch.cs
+++ Swan_Blindly/Assets/Vinnaeysh/Scripts/Camera/New Test Scripts/CineTOuch.cs
@@ -3,17 +3,22 @@ using Cinemachine;
 
 public class CineTOuch : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] CinemachineFreeLook cineCam;
-
     [SerializeField] CamTouchField touchField;
 
-    [SerializeField] float SensitivityX = 2f;
-    [SerializeField] float SensitivityY = 2f;
+    [Header("SensitivyController")]
+    [SerializeField] float SensitivityX = 0.1f;
+    [SerializeField] float SensitivityY = 0.1f;
+
+    [Header("SmoothenerController")]
+    [SerializeField] float XSmoothener = 10f;
+    [SerializeField] float YSmoothener = 0.1f;
     
     // Update is called once per frame
     void Update()
     {
-        cineCam.m_XAxis.Value += touchField.TouchDist.x *  SensitivityX * Time.deltaTime;
-        cineCam.m_YAxis.Value += touchField.TouchDist.y *  SensitivityY * Time.deltaTime;
+        cineCam.m_XAxis.Value += touchField.TouchDist.x *  SensitivityX * XSmoothener * Time.deltaTime;
+        cineCam.m_YAxis.Value += touchField.TouchDist.y *  SensitivityY * YSmoothener * Time.deltaTime;
     }
 }
