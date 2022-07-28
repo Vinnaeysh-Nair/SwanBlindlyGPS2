21c16dc9c536d39deb53c3d58270b2b8098e1da5
Chan Wing Shen <0128682@kdu-online.com>
2022-07-28 22:13:56 +0800

    dialogue fixed

--- Swan_Blindly/Assets/Wing Shen/Mushroom.cs
+++ Swan_Blindly/Assets/Wing Shen/Mushroom.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Mushroom : MonoBehaviour
 {
     Collider collider;
     public GameObject Dialogue;
-    public GameObject NameBox;
-    public GameObject TextBox;
+    public TextMeshProUGUI NameBox;
+    public TextMeshProUGUI TextBox;
     public int num;
     public Dialogue dialogue;
 
@@ -25,40 +26,40 @@ public class Mushroom : MonoBehaviour
             {
                 Dialogue.SetActive(true);
                 FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-                NameBox.GetComponent<Text>().text = "Tutorial 1";
-                TextBox.GetComponent<Text>().text = "Use the Joystick to move the character.";
+                NameBox.text = "Tutorial 1";
+                TextBox.text = "Use the Joystick to move the character.";
             }
             
             if(num == 2)
             {
                 Dialogue.SetActive(true);
                 FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-                NameBox.GetComponent<Text>().text = "Tutorial 2";
-                TextBox.GetComponent<Text>().text = "Swipe and hold on the right side of the screen to move the camera.";
+                NameBox.text = "Tutorial 2";
+                TextBox.text = "Swipe and hold on the right side of the screen to move the camera.";
             }
 
             if(num == 3)
             {
                 Dialogue.SetActive(true);
                 FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-                NameBox.GetComponent<Text>().text = "Tutorial 3";
-                TextBox.GetComponent<Text>().text = "Press the jump button to jump.";
+                NameBox.text = "Tutorial 3";
+                TextBox.text = "Press the jump button to jump.";
             }
 
             if(num == 4)
             {
                 Dialogue.SetActive(true);
                 FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-                NameBox.GetComponent<Text>().text = "Tutorial 4";
-                TextBox.GetComponent<Text>().text = "Go close to items to pick them up.";
+                NameBox.text = "Tutorial 4";
+                TextBox.text = "Go close to items to pick them up.";
             }
 
             if(num == 5)
             {
                 Dialogue.SetActive(true);
                 FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-                NameBox.GetComponent<Text>().text = "Tutorial 5";
-                TextBox.GetComponent<Text>().text = "";
+                NameBox.text = "Tutorial 5";
+                TextBox.text = "";
             }
         }
     }
