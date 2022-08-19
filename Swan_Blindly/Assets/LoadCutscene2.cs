09f2ad3639685a2da5cb147b3ae1cdd94d7bfe96
Vinnaeysh <0128028@kdu-online.com>
2022-08-19 10:30:16 +0800

    Added the cutscenes 2
    Need help connecting the final box collider to Cutscene 2. Does not want to load

---
+++ Swan_Blindly/Assets/LoadCutscene2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCutscene2 : MonoBehaviour
+{
+    [Header("Scene Switch Components")]
+    [SerializeField] private GameObject Cutscene2Loader;
+    CapsuleCollider COll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        COll = GetComponent<CapsuleCollider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Cutscene2Loader)
+        {
+            SceneManager.LoadScene("Cutscene 2");
+        }
+    }
+}
